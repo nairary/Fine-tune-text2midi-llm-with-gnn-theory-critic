3057f39d9c291f723b80d31d73ee22379013ee05
Valsamakin Aleksandr <valsamakin@cbi-info.ru>
2026-03-23 11:46:52 +0300

    fixed path issue

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from torch.utils.data import DataLoader
 from src.dataloader.hooktheory_dataset import HookTheoryDataset, collate_fn
 
-dataset = HookTheoryDataset("data/HTCanon/HK_processed/encoded_full/teacher_encoded.json")
+dataset = HookTheoryDataset("data/HTCanon/encoded_full/teacher_encoded.json")
 loader = DataLoader(dataset, batch_size=2, shuffle=True, collate_fn=collate_fn)
 
 for batch in loader:
