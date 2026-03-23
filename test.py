de7fda728c89ee49f90068bca783b809d86ba204
Alexander Valsamakin <77613731+nairary@users.noreply.github.com>
2026-03-23 15:03:45 +0300

    Refine teacher graph corruption and smoke stats

--- test.py
+++ test.py
@@ -1,16 +1,19 @@
 from torch.utils.data import DataLoader
+
+from src.dataloader.graph_layouts import NODE_DIMS
 from src.dataloader.hooktheory_dataset import HookTheoryDataset, collate_fn
 
-dataset = HookTheoryDataset("data/HTCanon/HK_processed/encoded_full/teacher_encoded.json")
+
+dataset = HookTheoryDataset("data/HTCanon/encoded_full/teacher_encoded.json")
 loader = DataLoader(dataset, batch_size=2, shuffle=True, collate_fn=collate_fn)
 
-for batch in loader:
-    print(batch["graph_real"])
-    print(batch["graph_masked"])
-    print(batch["graph_corrupted"])
-    print(batch["graph_real"]["note"].x.shape)
-    print(batch["graph_real"]["chord"].x.shape)
-    print(batch["graph_real"]["note", "next_note", "note"].edge_index.shape)
-    print(batch["graph_real"]["chord", "next_chord", "chord"].edge_index.shape)
-    print(batch["masked_labels"][0].keys())
-    break
+batch = next(iter(loader))
+graph = batch["graph_real"]
+
+print(graph)
+for node_type in ["song", "bar", "onset", "note", "chord"]:
+    print(f"{node_type}.x shape = {tuple(graph[node_type].x.shape)} | expected dim={NODE_DIMS[node_type]}")
+for edge_type in graph.edge_types:
+    print(f"{edge_type}: {tuple(graph[edge_type].edge_index.shape)}")
+print("covers_note edges:", graph[("chord", "covers_note", "note")].edge_index.shape)
+print(batch["masked_labels"][0].keys())
