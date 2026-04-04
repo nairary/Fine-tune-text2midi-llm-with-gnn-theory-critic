e064662f1e4cb54dda447284d6ca29286505c1cb
Alexander Valsamakin <77613731+nairary@users.noreply.github.com>
2026-04-04 15:56:55 +0300

    Refocus one-batch ablation on overfit-on-same-batch

--- scripts/run_ablation_one_batch.sh
+++ scripts/run_ablation_one_batch.sh
@@ -6,4 +6,4 @@ python src/training/train_teacher.py -m --config-name ablation_one_batch \
   model.use_hybrid_graph_scorer=false,true \
   model.pooling_mode=mean,mean_max \
   dataloader=graph_ablation,theory_aware_ablation \
-  dataloader.batch_size=1,16
+  dataloader.batch_size=16
